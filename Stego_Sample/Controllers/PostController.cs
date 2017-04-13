0b7fd68108c5be29679ac7d58bfb1e3ef66a5e3a
Ion Lica <ilica@endava.com>
2017-04-13 17:16:43 +0300

    Implemented method GetImage

--- Stego_Sample/Controllers/PostController.cs
+++ Stego_Sample/Controllers/PostController.cs
@@ -1,24 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Drawing;
+using System.IO;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Stego_Sample.Controllers
 {
     public class PostController : ApiController
     {
         [HttpPost]
-        public void RetrieveImage()
+        public void RetrieveImage(Stream stream)
         {
             
         }
 
         [HttpGet]
-        public void SendImage()
+        public void GetImage()
         {
-            
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = nullva
         }
     }
 }
