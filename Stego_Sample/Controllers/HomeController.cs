493001aeac780dbb6bea4c0ed857617b20706c3b
Ion Lica <licaion22@gmail.com>
2017-04-25 10:17:34 +0300

    Alpha Version 0.0.0.0.4

--- Stego_Sample/Controllers/HomeController.cs
+++ Stego_Sample/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using Newtonsoft.Json;
 using Stego_Sample.Models;
@@ -12,21 +17,47 @@ namespace Stego_Sample.Controllers
         private Bitmap _receivedBitmap;
 
         [HttpPost]
-        public void RetrieveImage(byte[] streamBytes, byte[] streamMessage)
-        {
-            var receivedMessage = Encoding.UTF8.GetString(streamMessage, 0, streamMessage.Length);
-            using (var mStream = new MemoryStream())
-            {
-                mStream.Write(streamBytes, 0, streamBytes.Length);
-                mStream.Seek(0, SeekOrigin.Begin);
-                var bm = new Bitmap(mStream);
-                _receivedBitmap = bm;
-            }
+		[Route("retrieveImage/{message}")]
+		public async Task<HttpResponseMessage> PostFile(string message)
+		{
+			HttpResponseMessage response = Request.CreateResponse();
+			if (!Request.Content.IsMimeMultipartContent())
+			{
+				throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+			}
 
-            SteganographyHelper.MergeText(receivedMessage, _receivedBitmap);
-        }
+			var rootDirectory = HttpContext.Current.Server.MapPath("~/Images");
+			var provider = new MultipartFormDataStreamProvider(rootDirectory);
 
-        [HttpGet]
+			try
+			{
+				await Request.Content.ReadAsMultipartAsync(provider);
+				File.Move(provider.FileData[0].LocalFileName, Path.Combine(rootDirectory, $"{message}.jpg"));
+				_receivedBitmap = Bitmap.FromFile($"{message}.jpg"); //TODO google it
+				response.StatusCode = HttpStatusCode.OK;
+				return null;
+			}
+			catch (Exception e)
+			{
+				return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+			}
+		}
+		//public void RetrieveImage(byte[] streamBytes, byte[] streamMessage)
+		//      {
+		//          var receivedMessage = Encoding.UTF8.GetString(streamMessage, 0, streamMessage.Length);
+		//          using (var mStream = new MemoryStream())
+		//          {
+		//              mStream.Write(streamBytes, 0, streamBytes.Length);
+		//              mStream.Seek(0, SeekOrigin.Begin);
+		//              var bm = new Bitmap(mStream);
+		//              _receivedBitmap = bm;
+		//          }
+
+		//          SteganographyHelper.MergeText(receivedMessage, _receivedBitmap);
+		//      }
+
+		[HttpGet]
+        [Route("getImage")]
         public string GetImage()
         {
             SteganographyHelper.ExtractText(_receivedBitmap);
